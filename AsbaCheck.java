ea6a71aa72e3b04b0cdc7f9699fc92ab6be7e8c6
Mukesh Prajapati <75403632+BakaYuki@users.noreply.github.com>
2025-03-17 17:55:43 +0545

    asba updated

--- AsbaCheck.java
+++ AsbaCheck.java
@@ -1,5 +1,7 @@
 package Meroshare;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -20,6 +22,8 @@ public class AsbaCheck extends BaseSetup {
 		// Clicking on Report
 		driver.findElement(By.xpath("(//button[@type='button'])[6]")).click();
 //		
+		
+		
 		Thread.sleep(5000);
         WebElement label = driver.findElement(By.xpath("(//div[@class='input-group'])[7]"));
         String labelText = label.getText();
@@ -27,47 +31,67 @@ public class AsbaCheck extends BaseSetup {
         // Print the text
         System.out.println("Label Text: " + labelText);
 
-
-
-		
-		// Getting the status
-//		List<WebElement> labels = driver.findElements(By.xpath("//div[contains(@class, 'input-group')]/label"));
-//		WebElement statusLabel = labels.get(6);
-//		String StatusText = "";
-//		StatusText =  statusLabel.getText();
-//		System.out.println("Status: " + StatusText);
-//		String text = "";
-//		for (WebElement label : labels) {
-//		    text = label.getText();
-//		    System.out.println("Status: " + text);
-//		}
 		
 	}
 	@Test
 	public static void AllotCheckLoop() throws InterruptedException{
-		for (int i =6; i<=35;i++) {
+		int alloted = 0;
+		int verified = 0;
+		int notalloted = 0;
+		
+		
+		
+		//MYASBA Click
+		driver.findElement(By.cssSelector("a[href='#/asba']")).click();
+		
+		// Application Report Click
+		driver.findElement(By.xpath("//main[@id='main']//li[3]//a[1]")).click();
+		Thread.sleep(2000);
+		
+        // Find all elements with the class 'company-list'
+        List<WebElement> companyList = driver.findElements(By.className("company-list"));
+
+        // Get the total number of companies
+        int totalCompanies = companyList.size();
+        totalCompanies = 6 + totalCompanies;
+        System.out.println("Total number of companies: " + totalCompanies);
+		
+		for (int i =6; i<totalCompanies;i++) {
 			//MYASBA Click
 			driver.findElement(By.cssSelector("a[href='#/asba']")).click();
 			
 			// Application Report Click
 			driver.findElement(By.xpath("//main[@id='main']//li[3]//a[1]")).click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			
 			driver.findElement(By.xpath("(//button[@type='button'])["+i+"]")).click();
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 			
 			WebElement label = driver.findElement(By.xpath("(//div[@class='input-group'])[7]"));
-	        String labelText = label.getText();
-
+	        String statusText = label.getText();
+	        if (statusText.equals("Verified")) {
+	        	verified +=1;
+	        	
+	        }
+	        else if(statusText.equals("Alloted")) {
+	        	alloted +=1;
+	        }
+	        else {
+	        	notalloted +=1;
+	        }
 	        // Print the text
-	        System.out.println("Label Text: " + labelText);
+//	        System.out.println("Label Text: " + statusText);
 			
 			
 			//Navigate Back
 			driver.navigate().back();
 
 		}
-        
+		System.out.println("Alloted: " + alloted);
+		System.out.println("Not Alloted: " + notalloted);
+		System.out.println("Verified: " + verified);
+        Thread.sleep(5000);
+		driver.close();
 	}
 
 }
