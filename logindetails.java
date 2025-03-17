f5a40194d9912de1b06da8871c889d4fe02575e5
Mukesh Prajapati <75403632+BakaYuki@users.noreply.github.com>
2025-03-17 17:07:38 +0545

    login info

---
+++ logindetails.java
@@ -0,0 +1,10 @@
+package Meroshare;
+
+public class logindetails {
+	public static String username = "";
+	public static String password = "";
+	public static String crn = "";
+	public static String pin = "";
+	public static String bank = "11000"; // Bank Unique Number This number is of NMB Bank
+
+}
