a9bf7f215e184f96c27b6ae9185d3adca23e571d
Mukesh Prajapati <75403632+BakaYuki@users.noreply.github.com>
2025-03-18 10:36:10 +0545

    Add files via upload

---
+++ manylogindetails.java
@@ -0,0 +1,10 @@
+package Meroshare;
+
+public class manylogindetails {
+	public static String[] username = {"first_acc_username","second","third"};
+	public static String[] password = {"first_acc_password","second","third"};
+	public static String[] crn = {"first_acc_CRN number","second","third"};
+	public static String[] pin = {"first_acc_Pin","second","third"};
+	public static String[] bank = {"first_acc_Bank_ number","second","third"}; // 11000 for NMB
+
+}
